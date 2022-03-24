e40288d48281522a0b5adbcf7b21dab64a6b3a6a
Manar <manarabdulrahman@outlook.sa>
2022-03-24 16:48:28 +0300

    fix routing

--- app/routes/article.routes.js
+++ app/routes/article.routes.js
@@ -4,13 +4,23 @@ module.exports = app => {
   var router = require("express").Router();
 
   // Create a new articles
-  router.post("/", articles.create);
+  //router.post("/", articles.create);
+  router.post("/", function (req, res) {
+    articles.create
+  });
+
 
   // Retrieve all articles
-  router.get("/", articles.findAll);
+  //router.get("/", articles.findAll);
+  router.get("/", function (req, res) {
+    articles.findAll
+  });
 
   // Retrieve all published articles
-  router.get("/available", articles.findAllAvailable);
+  //router.get("/available", articles.findAllAvailable
+  router.get("/available", function (req, res) {
+    articles.findAllAvailable
+  });
 
   app.use("/api/articles", router);
 };
